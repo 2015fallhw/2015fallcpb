ed7ddb780afccf78c01df7e7fe41894e77f80064
40423226 <40423226@gm.nfu.edu>
2015-11-17 00:49:46 +0800

    40423226

--- user/40423226/pelican/publishconf.py
+++ user/40423226/pelican/publishconf.py
@@ -26,8 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
-
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423226作業倉儲', 'http://40423226.github.io/CPB_40423226/'))
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('github', 'http://github.com/ametaireau'),)

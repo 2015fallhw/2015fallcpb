7ecf7df603a4bf6c164d213ff08d7940e77d4938
40423245 <40423245@gm.nfu.edu.tw>
2016-01-04 04:42:52 +0800

    add date

--- user/40423245/pelican/publishconf.py
+++ user/40423245/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40423212'
-SITENAME = '2015FALL 40423212 CPB 作業'
+AUTHOR = '40423245'
+SITENAME = '2015FALL 40423245 CPB 作業'
 SITEURL = ''
 
 PATH = 'content'

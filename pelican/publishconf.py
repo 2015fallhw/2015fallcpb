f392fbce6ef91d8e1f79097215b0fb20dde98213
40423212 <40423212@gm.nfu.edu.tw>
2016-01-07 19:28:57 +0900

    g1

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40423212'
-SITENAME = '2015FALL 40423212 CPB 作業'
+AUTHOR = '2015FALLCPB'
+SITENAME = '2015FALL CPB 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,12 +26,11 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),
-         ('倉儲','http://40423212.github.io/cpbtest2/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('github', 'http://github.com/ametaireau'),)
+          ('github', 'http://github.com/ametaireau'))
           
 DEFAULT_PAGINATION = 10
 
@@ -54,5 +53,3 @@ PLUGIN_PATHS = ['./../../../plugin']
 PLUGINS = ['liquid_tags.notebook']
 # 目錄設定相對於 reveal 下的 content 目錄
 NOTEBOOK_DIR = 'notebook'
-
-

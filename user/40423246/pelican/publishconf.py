0f23c0bd88a7e9a34dcf57fc97e033e258495c9e
40423212 <40423212@gm.nfu.edu.tw>
2016-01-14 21:26:25 +0900

    46

--- user/40423246/pelican/publishconf.py
+++ user/40423246/pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40423246'
-SITENAME = '2015FALL 40423246 CADPA 作業'
+SITENAME = '2015FALL 40423246 CPB 作業'
 SITEURL = ''
 
 PATH = 'content'

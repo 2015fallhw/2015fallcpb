782105300eb0739ab1af1b7558adad5393592329
40423248 <40423248@gm.nfu.edu.tw>
2016-01-15 13:09:57 +0800

    io.test

--- user/40423248/pelican/publishconf.py
+++ user/40423248/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423248倉儲','http://40423248.github.io/cpbw3/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423248 io.倉儲','http://40423248.github.io/test-all//'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),

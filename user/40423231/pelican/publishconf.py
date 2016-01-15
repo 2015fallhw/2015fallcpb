e8111387f38c26d14f4fa738ecbe3ec8a57df7a0
40423223 <40423223@gm.nfu.edu.tw>
2016-01-16 02:54:25 +0800

    31

--- user/40423231/pelican/publishconf.py
+++ user/40423231/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423231 個人網頁','http://40423225.github.io/cpbtest2/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423231 個人網頁','http://40423231.github.io/cpbw1//'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
